ca747425d3ea6c1f1b0e0208cda6a887893071eb
fil303 <55945436+fil303@users.noreply.github.com>
2020-12-18 15:22:34 +0000

    Add files via upload

---
+++ auth/middleware_admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->session()->exists('login_web_59ba36addc2b2f9401580f014c7f58ea4e30989d')) {
+            return  redirect('home');
+        }
+        return $next($request);
+    }
+}
